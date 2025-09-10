0554d84b39798192fdea1cdb2a8483e8b7d6895c
Amir-h89 <amir.h.mohammadaz@gmail.com>
2025-09-10 16:01:12 +0330

    Add files via upload

---
+++ bakhsh paziri.py
@@ -0,0 +1,21 @@
+n = int(input('enter the number: '))
+
+if n % 2 == 0:
+    print('on 2')
+if n % 3 == 0:
+    print('on 3')
+if n % 4 == 0:
+    print('on 4')
+if n % 5 == 0:
+    print('on 5')
+if n % 6 == 0:
+    print('on 6')
+if n % 7 == 0:
+    print('on 7')
+if n % 8 == 0:
+    print('on 8')
+if n % 9 == 0:
+    print('on 9')
+if n % 10 == 0:
+    print('on 10')
+    
