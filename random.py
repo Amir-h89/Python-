2e5a387ddaf550fdfa25f1988cab13507b911f02
Amir-h89 <amir.h.mohammadaz@gmail.com>
2025-09-10 16:04:43 +0330

    Add files via upload

---
+++ random.py
@@ -0,0 +1,12 @@
+x = int(input('enter the number of poeple: '))
+
+import random
+
+while True:
+    s = input(':')
+    if s == 'break':
+        break
+    else:
+        n = random.randint(1, x,)
+    print(n)
+    
