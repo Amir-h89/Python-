3b479570022abb1d3b097c02af7f65d968725ccc
Amir-h89 <amir.h.mohammadaz@gmail.com>
2025-09-10 16:03:57 +0330

    Add files via upload

---
+++ Ques_3.py
@@ -0,0 +1,44 @@
+print('1_the number of olympic rings?')
+print('1: 6')
+print('2: 5')
+print('3: 7')
+print('4: 4')
+
+an = int(input('enter the correct answer: '))
+
+if an == 2:
+    an = True
+else:
+    an = False    
+
+print('2_how many contients are there?')
+print('1: 2')
+print('2: 4')
+print('3: 5')
+print('4: 3')
+
+a = int(input('enter the correct answer: '))
+
+if a == 3:
+    a = True
+else:
+    a = False
+
+print('3_who discovered alcohol?')
+print('1: zakaria razi')
+print('2: abu ali sina')
+print('3: edison')
+print('4: ferdosi')
+
+b = int(input('enter the correct answer: '))
+
+if b == 1:
+    b = True
+else:
+    b = False
+
+print('1:', an)
+print('2:', a)
+print('3:', b)
+
+
